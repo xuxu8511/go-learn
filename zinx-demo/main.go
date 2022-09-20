071c8d205f9016b008a3979cdb9bdf77ac3bf8c3
xuxu8511 <xuxu8511@163.com>
2022-09-20 13:05:03 +0800

    first commit

--- zinx-demo/main.go
+++ zinx-demo/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"zinx-demo/global"
+	. "zinx-demo/global"
 	. "zinx-demo/xnet"
 )
 
 func main() {
-	s := NewServer("tcpserver", "tcp4", global.G_ZinxConfig.Server.Ip, int32(global.G_ZinxConfig.Server.Port))
+	s := NewServer("tcpserver", "tcp4", G_ZinxConfig.Server.Ip, int32(G_ZinxConfig.Server.Port))
 
 	routerManager := NewRouterManager()
 	routerManager.AddRouter(1, &BaseRouter{})
